4317410c3d96185b148f6c80dd86c3607f229767
Carlos Andres Cuero <suazass@hotmail.com>
2026-05-14 12:11:57 -0500

    Proyecto base

---
+++ src/main/java/com/practice/franquicias_management_api/FranquiciasManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.practice.franquicias_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciasManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciasManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/franquicias_management_api/FranquiciasManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.franquicias_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
